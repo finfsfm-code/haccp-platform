e3f8206eb8722d3e85718b89be2c67c8bd772137
Food Saftey <finfsfm@gmail.com>
2025-10-28 16:26:05 +0300

    test4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-// vite.config.ts
+
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// التعديل الحاسم: تحديد المسار الأساسي لـ GitHub Pages.
+// هذا الإعداد هو المفتاح: يوجه Vite لاستخدام اسم المستودع كمسار أساسي (Base Path)
 export default defineConfig({
   base: '/haccp-platform/',
   plugins: [react()],
