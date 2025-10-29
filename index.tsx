98a58f6bdbf779e24bb127b28d9226c7c03dd1db
Food Saftey <finfsfm@gmail.com>
2025-10-29 16:54:40 +0300

    test136

--- index.tsx
+++ index.tsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 // FIX: Added .tsx extension to resolve module.
 import { AppProvider } from './context/AppContext.tsx';
-
+import './index.css';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
